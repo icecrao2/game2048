af0b73283e3489628c5e86c99c34e09e41ef2669
jaewoong <icecrao2@daum.net>
2023-09-03 12:21:59 +0900

    init

---
+++ game2048/game2048App.swift
@@ -0,0 +1,17 @@
+//
+//  game2048App.swift
+//  game2048
+//
+//  Created by sw on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct game2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
