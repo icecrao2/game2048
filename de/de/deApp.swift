552d951362a0f5f19312e4adb254f615eb912686
icecrao2 <icecrao2@daum.net>
2023-10-16 22:19:19 +0900

    xcode bug setting

---
+++ de/de/deApp.swift
@@ -0,0 +1,17 @@
+//
+//  deApp.swift
+//  de
+//
+//  Created by sw on 2023/10/15.
+//
+
+import SwiftUI
+
+@main
+struct deApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
