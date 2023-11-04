7e0ab84e361e6178faab13aa6987bb64cc29b037
icecrao2 <icecrao2@daum.net>
2023-11-04 18:22:51 +0900

    admob banner id 리소스화

---
+++ game2048/Resources/AdMobResources.swift
@@ -0,0 +1,16 @@
+//
+//  AdMobResources.swift
+//  game2048
+//
+//  Created by sw on 2023/11/04.
+//
+
+import Foundation
+
+
+struct AdMobResource {
+    
+    static let BANNER_ID = "ca-app-pub-2621096690952385/1577372838"
+    static let TEST_BANNER_ID = "ca-app-pub-3940256099942544/2934735716"
+    
+}
